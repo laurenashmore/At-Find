b1d8f4f7c9a66512116e0c3c9498e65f99888a10
laurenashmore <85303657+laurenashmore@users.noreply.github.com>
2021-07-13 10:42:40 +0100

    start service

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:atfind/screens/onboarding.dart';
+import 'package:atfind/screens/profile.dart';
+import 'package:atfind/screens/Settings.dart';
+import 'package:atfind/screens/SendAlert.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FIND',
+      //navigatorKey: NavService.navKey,
+      theme: ThemeData(
+        primaryColor: Colors.grey[400],
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: OnboardingScreen.id,
+      routes: {
+        OnboardingScreen.id: (context) => OnboardingScreen(),
+        Home.id: (context) => Home(),
+        profile.id: (context) => profile(),
+        MyContactsScreen.id: (context) => MyContactsScreen(),
+        Settings.id: (context) => Settings(),
+        SendAlert.id: (context) => SendAlert(),
+
+      },
+    );
+  }
+}

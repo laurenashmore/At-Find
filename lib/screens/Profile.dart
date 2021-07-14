828bdd3d03cfb4c43aa92f798a4af933d9e4f229
RanchaoO <ranchao088@gmail.com>
2021-07-14 14:27:23 +0100

    fixed gradle error issues

--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -120,7 +120,7 @@ class _ProfileState extends State<Profile> {
   }
 
 
-  /// Look up a value corresponding to an [AtKey] instance.
+/// Look up a value corresponding to an [AtKey] instance.
 /*  Future<String> _lookup(AtKey atKey) async {
     ClientService clientSdkService = ClientService.getInstance();
     // If an AtKey object exists
@@ -130,44 +130,34 @@ class _ProfileState extends State<Profile> {
     }
     return '';
   }
-
-
   /// Scan for [AtKey] objects with the correct regex.
   _scan() async {
     ClientService clientSdkService = ClientService.getInstance();
     // Instantiate a list of AtKey objects to house each cached recipe from
     // the secondary server of the authenticated atsign
-
     List<AtKey> response;
-
     // This regex is defined for searching for an AtKey object that carries the
     // namespace of cookbook and that have been created by the authenticated
     // atsign (the currently logged in atsign)
     String? regex = '^(?!cached).*atfind.*';
-
     // Getting the recipes that are cached on the authenticated atsign's secondary
     // server utilizing the regex expression defined earlier
     response = await clientSdkService.getAtKeys(regex:'^(?!cached).*atfind.*' );
     response.retainWhere((element) => !element.metadata!.isCached);
-
     // Instantiating a list of strings
     List<String> responseList = [];
-
     // Looping through every instance of an AtKey object
     for (AtKey atKey in response) {
       // We get the current AtKey object that we are looping on
       String value = await _lookup(atKey);
-
       // In addition to the object we are on, we add the name of the recipe,
       // the constant splitter to segregate the fields, and again, the value of
       // the recipe which includes; description, ingredients, and image URL
       value = (atKey.key! + value);
-
       // Add current AtKey object to our list of strings defined earlier before
       // for loop
       responseList.add(value);
     }
-
     // After successfully looping through each AtKey object instance,
     // return list of strings
     //return responseList;
